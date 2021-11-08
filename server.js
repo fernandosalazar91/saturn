8544e69dfe53295caae6fe77118499f37f6f8aca
Fernando Salazar <fernando.salazar@quarkworks.co>
2021-11-08 10:12:21 -0800

    endpoint update

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios');
+const axios = require('axios').default;
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
+
+server.get('/assets', (req, res) => {
+    const { address } = req.body;
+
+    const options = {
+        method: 'GET',
+        url: 'https://api.opensea.io/api/v1/assets?owner=0xce6c126eeec5abca0e6a80653a0b693051f55cda&order_direction=desc&offset=0&limit=20'
+    };
+
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+});
 
 module.exports = server;
